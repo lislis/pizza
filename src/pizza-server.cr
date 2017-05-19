02f446e5469ea4b04c5d5a447fcbe43e763ee569
lislis <mail@lislis.de>
2017-05-19 11:27:22 +0200

    change api

--- src/pizza-server.cr
+++ src/pizza-server.cr
@@ -1,8 +1,9 @@
+# coding: utf-8
 require "./pizza-server/*"
 require "kemal"
 require "json"
 
-get "/" do |env|
+get "/pizza" do |env|
   env.response.content_type = "application/vnd.api+json"
   env.response.headers.add "Access-Control-Allow-Origin", "*"
   {data: [
@@ -11,6 +12,17 @@ get "/" do |env|
     {name: "Il Casolare", address: "Grimmstr. 30, 10967 Berlin, Germany"},
     {name: "Stranero", address: "Liebenwalderstrasse 11, 13347 Berlin, Germany"}
   ]}.to_json
+end
+
+get "/kieze" do |env|
+  env.response.content_type = "application/vnd.api+json"
+  env.response.headers.add "Access-Control-Allow-Origin", "*"
+  {data: [
+     {name: "Wedding", long: "technically Mitte"},
+     {name: "Pberg", long: "technically Pankow"},
+     {name: "Fhain+Kberg", long: "Friedrichshain-Kreuzberg"},
+     {name: "Neukölln", long: "just Neukölln"}
+   ]}.to_json
 end
 
 Kemal.run
