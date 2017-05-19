b033aa22d817ad7b3655db99784fc1165c3a9e0e
lislis <mail@lislis.de>
2017-05-19 11:38:27 +0200

    chnge again

--- src/pizza-server.cr
+++ src/pizza-server.cr
@@ -7,10 +7,19 @@ get "/pizza" do |env|
   env.response.content_type = "application/vnd.api+json"
   env.response.headers.add "Access-Control-Allow-Origin", "*"
   {data: [
-    {name: "I Due Forni", address: "Schoenhauser Allee 12, 10119 Berlin, Germany"},
-    {name: "Il Ritrovo", address: "Gabriel-Max-Str. 2, 10245 Berlin, Germany"},
-    {name: "Il Casolare", address: "Grimmstr. 30, 10967 Berlin, Germany"},
-    {name: "Stranero", address: "Liebenwalderstrasse 11, 13347 Berlin, Germany"}
+     {name: "I Due Forni",
+      address: "Schoenhauser Allee 12, 10119 Berlin, Germany",
+      kiez: "Pberg"},
+     {name: "Il Ritrovo",
+      address: "Gabriel-Max-Str. 2, 10245 Berlin, Germany",
+       kiez: "Fhain+Xberg"},
+     {name: "Sfizy Veg",
+      address: "Treptower Str. 95, 12059 Berlin, Germany",
+      kiez: "Neukölln"
+     },
+     {name: "Stranero",
+      address: "Liebenwalderstrasse 11, 13347 Berlin, Germany",
+      kiez: "Wedding"}
   ]}.to_json
 end
 
@@ -20,7 +29,7 @@ get "/kieze" do |env|
   {data: [
      {name: "Wedding", long: "technically Mitte"},
      {name: "Pberg", long: "technically Pankow"},
-     {name: "Fhain+Kberg", long: "Friedrichshain-Kreuzberg"},
+     {name: "Fhain+Xberg", long: "Friedrichshain-Kreuzberg"},
      {name: "Neukölln", long: "just Neukölln"}
    ]}.to_json
 end
